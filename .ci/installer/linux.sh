831bf9fa86d2b8eb8219a2a8279b05fb38f0d3f6
haya14busa <hayabusa1419@gmail.com>
2016-09-29 23:27:48 +0900

    remove exit

--- .ci/installer/linux.sh
+++ .ci/installer/linux.sh
@@ -3,7 +3,6 @@ tmp=$(mktemp -d)
 url=https://github.com/vim/vim
 git clone --depth 1 --single-branch $url $tmp
 cd $tmp
-exit
 ./configure --prefix="$HOME/vim" \
     --enable-fail-if-missing \
     --with-features=huge
